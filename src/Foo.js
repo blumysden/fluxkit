5494731ebc4c8e0e562beae567632fbb43f45403
Scott Blumenthal <scott.blumenthal@gmail.com>
2017-07-22 00:58:59 -0400

    fun stylez

--- src/Foo.js
+++ src/Foo.js
@@ -2,12 +2,28 @@ import React, { Component } from 'react';
 import UGC from './ugc'
 import './Foo.css';
 
+const examplePs = [
+  'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+  'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+]
+
 class Foo extends Component {
   render() {
-    console.log(this.props.match.params.id);
     return (
       <div className="activity">
-        <p>Here is some stuff we want to show just about the foo activity.</p>
+        { examplePs.map((p) => {
+          let rotation = Math.floor(Math.random() * 4),
+              indent = Math.floor(Math.random() * 50)
+
+          if (Math.round(Math.random()) == 0) {
+            rotation = rotation * -1
+          }
+          if (Math.round(Math.random()) == 0) {
+            indent = indent * -1
+          }
+          return <p className="special" style={ { transform: `rotate(${rotation}deg)`, marginLeft: `${indent}px` } }><span>{p}</span></p>
+        }) }
         <UGC sheetCol="4" />
       </div>
     );
