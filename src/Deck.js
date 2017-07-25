9fc82c92faa542f56acfaad48eb591377c6ed4c9
Scott Blumenthal <scott.blumenthal@nytimes.com>
2017-07-25 11:35:52 -0400

    keyboard navigation

--- src/Deck.js
+++ src/Deck.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 import $ from 'jquery'
 import './Deck.css';
 import archieml from 'archieml'
@@ -9,11 +10,46 @@ const SLIDES = archieml.load(deckContent).slides
 
 class Deck extends Component {
 
+  constructor(props) {
+    super(props)
+    this.handleKeys = this.handleKeys.bind(this)
+    this.state = {
+      slide: 1
+    }
+  }
+
   resize() {
     $(this.deck).height($(window).height())
   }
 
+  advance(by=1) {
+    let slide = this.state.slide + by
+    if (slide < 1) {
+      slide = 1
+    } else if (slide > SLIDES.length) {
+      slide = SLIDES.length
+    }
+    this.setState({ slide })
+  }
+
+  handleKeys(e) {
+    if (e && e.code) {
+      switch (e.code) {
+        case 'ArrowRight':
+        case 'ArrowDown':
+        case 'Space':
+          this.advance()
+          break
+        case 'ArrowUp':
+        case 'ArrowLeft':
+          this.advance(-1)
+          break;
+      }
+    }
+  }
+
   componentDidMount() {
+    document.addEventListener('keydown', this.handleKeys)
     this.resize()
   }
 
@@ -21,13 +57,25 @@ class Deck extends Component {
     this.resize()
   }
 
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeys)
+  }
+
   render() {
-    let { slide = 1 } = this.props.match.params,
-        data = SLIDES[slide - 1],
+    let { slide } = this.props.match.params
+    console.log(slide, this.state.slide, slide);
+    if (this.state.slide !== parseInt(slide)) {
+      return <Redirect push to={ `/deck/${this.state.slide}` } />
+    }
+
+    let data = SLIDES[slide - 1],
         classNames = ['slide']
+
     if (!data) {
       return null
     }
+
+
     let { hed, img, caption, content } = data
     if (img) {
       classNames.push('has-img')
--- src/deck-aml.js
+++ src/deck-aml.js
@@ -3,6 +3,7 @@ export default `
 
 [slides]
 
+----- SLIDE ------
 hed: new sNerdFluxkit(2017)
 
 img: fingerbox.jpg
@@ -13,7 +14,20 @@ This is a bunch of stuff
 
 It could run to several lines, I guess.
 :end
+----- END SLIDE ------
 
+----- SLIDE ------
+hed: What are we doing here?
+
+img: fingerbox.jpg
+caption: Ay-O's fingerbox
+
+content:
+This is a bunch of stuff
+
+It could run to several lines, I guess.
+:end
+----- END SLIDE ------
 
 []
 
