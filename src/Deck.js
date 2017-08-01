10994ee62f79b18909d78aa1c1c291512944f163
Scott Blumenthal <scott.blumenthal@nytimes.com>
2017-08-01 11:38:20 -0400

    starting on timer.  cancelling click advance for now, until we can isolate it a little

--- src/Deck.js
+++ src/Deck.js
@@ -52,9 +52,11 @@ class Deck extends Component {
   }
 
   handleClick(e) {
-    if (e) {
-      this.advance()
-    }
+    // TODO: I think we need to isolate this somehow, because
+    // right now clicking on a link or timer or whatever advances the slide
+    // if (e) {
+    //   this.advance()
+    // }
   }
 
   componentDidMount() {
@@ -163,21 +165,41 @@ class Timer extends Component {
     this.state = {
       min: 0,
       sec: 0,
+      remaining: parseInt(props.minutes) * 60,
       running: false
     }
+    this.interval = null
     this.toggle = this.toggle.bind(this)
   }
 
-  componentDidMount() {
-    this.setState({ min: this.props.minutes })
+  pause() {
+    window.clearInterval(this.interval)
+    this.setState({ counting: false })
   }
 
-  toggle () {
+  countdown() {
+    this.pause()
+    this.setState({ counting: true })
+    this.interval = window.setInterval(() => {
+      this.setState({ remaining: this.state.remaining - 1 })
+    }, 1000)
+  }
 
+  toggle (e) {
+    e.stopPropagation()
+    e.preventDefault()
+    if (this.state.running) {
+      this.pause()
+    } else {
+      this.countdown()
+    }
   }
 
   render() {
-    let { min, sec } = this.state
+    let { remaining } = this.state,
+        min = Math.floor(remaining / 60),
+        sec = remaining % 60
+
     if (sec < 10) {
       sec = `0${sec}`
     }
